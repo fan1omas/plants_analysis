bc9c668bcb26c1e45ecfd57ee0b37cc272f28cb7
fantomas <d2nishevsky@gmail.com>
2026-03-08 21:48:58 +0300

    data will be sent in project folder

--- src/download.py
+++ src/download.py
@@ -5,7 +5,18 @@ import shutil
 
 PROJECT_DIR = Path(__file__).parent.parent
 DATA_DIR = PROJECT_DIR / "data" / "raw"           # получаем путь к данным для обучения 
+DATA_DIR.mkdir(exist_ok=True, parents=True)
 
+path = kagglehub.dataset_download("abdallahalidev/plantvillage-dataset")
 
+color_folder = None
+for path in Path(path).rglob("*"):   # рекурсивный поиск по всем папкам
+    if path.is_dir() and path.name == "color":
+        color_folder = path
+        break
 
+target_path = DATA_DIR / "plantvillage" / "color"
+shutil.copytree(color_folder, target_path)
+shutil.rmtree(path)
 
+print('done')
