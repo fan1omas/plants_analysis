25cf156073def7e67aa12bda51922aa531438355
fantomas <d2nishevsky@gmail.com>
2026-03-08 19:56:18 +0300

    created download.py

---
+++ src/download.py
@@ -0,0 +1,11 @@
+import kagglehub
+from pathlib import Path
+import shutil
+
+
+PROJECT_DIR = Path(__file__).parent.parent
+DATA_DIR = PROJECT_DIR / "data" / "raw"           # получаем путь к данным для обучения 
+
+
+
+
